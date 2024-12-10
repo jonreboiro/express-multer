035fe1710579fa94fb0cf61653119392980bdd79
jonreboiro <157797191+jonreboiro@users.noreply.github.com>
2024-12-10 09:29:45 +0000

    version 1

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,52 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const path = require('path'); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.redirect('form.html');
 });
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      const ext = path.extname(file.originalname);
+      cb(null, file.fieldname + '-' + uniqueSuffix + ext)
+    }
+  })
+
+const fileFilter = function (req, file, cb) {
+    const allowedMimeTypes = ['image/png', 'image/jpeg'];
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Only PNG files are allowed'), false);
+    }
+};
+  
+const limits = {
+    fileSize: 2 * 1024 * 1024
+};
+  
+const upload = multer({ 
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: limits
+})
+
+
 router.post('/', upload.single('avatar'), function (req, res, next) {
-    console.log(req.file)
-    // req.body will hold the text fields, if there were any
-    res.send("Jasota")
+    const name = req.body.name;
+    const filePath = `/uploads/${req.file.filename}`;
+
+    res.send(`
+        <p>Zure izena: ${name}</p>
+        <p>Fitxategia: <a href="${filePath}" target="_blank">${filePath}</a></p>
+      `);
 })
 
 
